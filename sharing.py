2ecd239f153e129efc304b2cbdc41907ba3df882
Mara <mara-li@outlook.fr>
2021-09-01 14:26:41 +0200

    ✨ Convert 100% Obsidian using python

---
+++ sharing.py
@@ -0,0 +1,325 @@
+#!/usr/bin/env python3
+
+import re
+import sys
+import os
+from dotenv import dotenv_values
+from pathlib import Path
+import shutil
+
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+env = dotenv_values(Path(f"{BASEDIR}/.env"))
+path = Path(f"{BASEDIR}/.git")  # GIT SHARED
+vault = Path(env["vault"])
+print(vault)
+site = env["site"]
+post = Path(f"{BASEDIR}/_notes")
+img = Path(f"{BASEDIR}/assets/img/")
+
+# todo: delete comments ?
+# todo: add side support with image
+# todo: add hightlight support
+
+
+def get_token_end(final_text):
+    token = ""
+    if re.search(".+\{(.*)\}", final_text):
+        token = re.search(".+\{(.*)\}", final_text)
+        token = token.group(1)
+        token = " {" + token + "}"
+    return token
+
+
+def get_token_start(final_text):
+    token = ""
+    if final_text.startswith("{"):
+        token = re.search("\{(.*)\}.+", final_text)
+        token = token.group(1)
+        token = "{" + token + "} "
+    return token
+
+
+def get_table_end(final_text):
+    table = ""
+    if re.search("(\]{2}|\))(.*)", final_text):
+        tab = re.search("(\]{2}|\))(.*)", final_text)
+        tab = tab.group(0)
+        if "|" in tab:
+            table = " |"
+    return table
+
+
+def get_start_table(final_text):
+    table = ""
+    if re.search("\|[\[|\(](.*)", final_text):
+        tab = re.search("\|[\[|\(](.*)", final_text)
+        tab = tab.group(0)
+        if "|" in tab:
+            table = "| "
+    return table
+
+
+def get_tab_token(final_text):
+    token_end = get_token_end(final_text)
+    table_end = get_table_end(final_text)
+    table_start = get_start_table(final_text)
+    token_start = get_token_start(final_text)
+    return token_end, table_end, table_start, token_start
+
+
+def delete_file(filepath):
+    for file in os.listdir(post):
+        print(file)
+        filepath = os.path.basename(filepath)
+        filecheck = os.path.basename(file)
+        if filecheck == filepath:
+            os.remove(Path(f"{BASEDIR}\_notes\{file}"))
+            return True
+    return False
+
+
+def get_image(image):
+    for sub, dirs, files in os.walk(vault):
+        for file in files:
+            filepath = sub + os.sep + file
+            if image in file:
+                return filepath
+
+def move_img(line):
+    token, table, table_start, token_start = get_tab_token(line)
+    img_flags = re.search("\|(.*)(]{2}|\))", line)
+    if img_flags:
+        img_flags=img_flags.group(0)
+        img_flags=img_flags.replace("|", "")
+        img_flags=img_flags.replace("]", "")
+        img_flags=img_flags.replace(")", "")
+    else:
+        img_flags=""
+    final_text = re.search("(\[{2}|\().*\.(png|jpg|jpeg|gif)", line)
+    final_text = final_text.group(0)
+    final_text = final_text.replace("(", "")
+    final_text = final_text.replace("%20", " ")
+    final_text = final_text.replace("[", "")
+    final_text = final_text.replace("]", "")
+    image_path = get_image(final_text)
+    if image_path:
+        shutil.copyfile(image_path, f"{img}/{final_text}")
+        final_text = f"../assets/img/{final_text}"
+        final_text = (
+            f"{table_start}{token_start}![{img_flags}]({final_text}){token}{table}  \n  \n"
+        )
+    else:
+        final_text = line
+    return final_text
+
+
+def relative_path(data):
+    data = data.rstrip() + ".md"
+    for sub, dirs, files in os.walk(vault):
+        for file in files:
+            filepath = sub + os.sep + file
+            if data == file:
+                return filepath
+
+
+def check_file(filepath):
+    check = ""
+    for file in os.listdir(post):
+        if filepath == file:
+            check = "EXIST"
+    return check
+
+
+def dest(filepath):
+    file_name = os.path.basename(filepath)
+    dest = Path(f"{BASEDIR}/_notes/{file_name}")
+    return str(dest)
+
+
+def convert_internal(line):
+    ft = line.replace("[", "")
+    ft = ft.replace("]", "")
+    ft = ft.replace("!", "")
+    ft = ft.replace("(", "")
+    ft = ft.replace(")", "")
+    ft = ft.rstrip()
+    file = relative_path(ft)
+    destination = f"{BASEDIR}/_notes/{ft}"
+    line_final = ""
+    if file:
+        if file.endswith(f"{ft}.md"):
+            file = os.path.basename(file)
+            check = check_file(file)
+            share = check_share(file)
+            if share:
+                if check != "EXIST":
+                    file_convert(file)
+            line_final = f"[[{destination}\|{ft}]]"
+    return line_final
+
+
+def file_convert(file):
+    file_name = os.path.basename(file)
+    print(file)
+    if not "_notes" in file:
+        if check_file(file_name) != "EXIST":
+            data = open(file, "r", encoding="utf-8")
+            final = open(Path(f"{BASEDIR}/_notes/{file_name}"), "w", encoding="utf-8")
+            lines = data.readlines()
+            data.close()
+            for ln in lines:
+                final_text = ln.replace("\n", "  \n")
+                try:
+                    if re.search("\%\%(.*)\%\%", final_text):
+                        #remove comments
+                        final_text="  \n"
+                    elif re.search("==(.*)==", final_text):
+                        final_text=re.sub("( ?)==", "[[", final_text, 1)
+                        final_text=re.sub("( ?)==", "::highlight]]", final_text, 2)
+                    elif re.search(
+                        "(\[{2}|\().*\.(png|jpg|jpeg|gif)", final_text
+                    ):  # CONVERT IMAGE
+                        final_text = move_img(final_text)
+                    elif (
+                        "\\" in final_text.strip()
+                    ):  # New line when using "\n" in obsidian file
+                        final_text = "  "
+                    elif re.search("(\[{2}|\().*", final_text):
+                        # Add internal_link to blog too !
+                        ft = re.search("(\[{2}|\().*", final_text)
+                        ft = ft.group(0)
+                        link = convert_internal(ft)
+                        token, table, table_start, token_start = get_tab_token(
+                            final_text
+                        )
+                        final_text = (
+                            table_start
+                            + token_start
+                            + final_text
+                            + token
+                            + table
+                            + "  \n"
+                        )
+                except TypeError:
+                    final_text = ln + "  \n"
+                final.write(final_text)
+            final.close()
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def search_share(option=0):
+    index = []
+    for sub, dirs, files in os.walk(vault):
+        for file in files:
+            filepath = sub + os.sep + file
+            if filepath.endswith(".md"):
+                data = open(filepath, "r", encoding="utf-8")
+                for ln in data.readlines():
+                    if "share: true" in ln:
+                        if option == 1:
+                            delete_file(filepath)
+                        check = file_convert(filepath)
+                        destination = dest(filepath)
+                        if check:
+                            index.append(destination)
+            return index
+
+
+def check_share(file):
+    share = True
+    f = open(file, "r", encoding="utf-8")
+    if "share: false" in f.readlines():
+        share = False
+    return share
+
+
+def convert_to_github():
+    """
+    Create file in _notes, move image in assets, convert to relative path, add share support, and push to git
+    ----
+    Usage
+    -----
+        python3 sharing (filepath) (options)
+        Optional option:
+            - --d : Delete file if already exist (update)
+            - help : print help message
+            - filepath: convert just one file
+            - --ng : no commit and no push to github.
+    """
+    if len(sys.argv) >= 2:
+        if sys.argv[1] == "help":
+            print(help(convert_to_github))
+        else:
+            print("Starting convert")
+            ori = sys.argv[1]
+            delopt = ""
+            ng = ""
+            if "--d" in sys.argv:
+                delopt = "--d"
+            if "--ng" in sys.argv:
+                ng = "--ng"
+            if os.path.exists(ori) and ori != "--d":
+                delete_file(ori)
+                check = file_convert(ori)
+                if check and ng != "--ng":
+                    COMMIT = f"{dest} to blog"
+                    try:
+                        from git import Repo
+
+                        repo = Repo(Path(f"{BASEDIR}/.git"))
+                        destination = dest(ori)
+                        repo.index.add(destination)
+                        repo.index.commit(COMMIT)
+                        origin = repo.remote(name="origin")
+                        origin.push()
+                        print("File pushed successfully 🎉")
+                    except ImportError:
+                        print("Please, use Working Copy to push your change")
+                elif delopt == "--d":
+                    new_files = search_share(1)
+                    commit = "Add to blog:"
+                    if len(new_files) > 0 and ng != "--ng":
+                        try:
+                            from git import Repo
+
+                            repo = Repo(path)
+                            for md in new_files:
+                                if os.path.exists(Path(f"{BASEDIR}/_notes/{md}")):
+                                    commit = commit + "\n — " + md
+                                repo.index.add(f"{BASEDIR}/_notes/{md}")
+                            repo.index.commit(commit)
+                            origin = repo.remote(name="origin")
+                            origin.push()
+                            print(f"\n{commit} pushed successfully 🎉")
+                        except ImportError:
+                            print("Please use Working Copy to push your project")
+    else:
+        print("Starting Convert")
+        new_files = search_share()
+        commit = "Add to blog :"
+        if len(new_files) > 0:
+            try:
+                from git import Repo
+                repo = Repo(path)
+                for md in new_files:
+                    if os.path.exists(Path(f"{BASEDIR}/_notes/{md}")):
+                        commit = commit + "\n — " + md
+                        repo.index.add(f"{BASEDIR}/_notes/{md}")
+                repo.index.commit(commit)
+                origin = repo.remote(name="origin")
+                origin.push()
+                print(f"{commit} pushed successfully 🎉")
+            except ImportError:
+                print("Please use working copy")
+        else:
+            print("File already exists 😶")
+
+
+if __name__ == "__main__":
+    convert_to_github()
+
